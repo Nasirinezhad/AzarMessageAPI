74e1d9eba2b6f44dc6a37eb9e850567c77bf425c
Nasirinezhad <marsav98@gmail.com>
2022-12-15 15:26:57 +0330

    Build xml body and send to server methods

--- src/AzarSMS.php
+++ src/AzarSMS.php
@@ -4,5 +4,34 @@ namespace Nasirinezhad\AzarmessageApi;
 
 class AzarSNS
 {
-    
+    private $userid = '';
+    private $password = '';
+    private $action = 'smssend';
+
+    public function __construct($user, $password) {
+        $this->userid = $user;
+        $this->password = $password;
+    }
+
+    public function send(Message $message)
+    {
+        $client = new nusoap_client($url, 'wsdl');
+        $client->soap_defencoding = 'UTF-8';
+        $client->decode_utf8 = true;
+        $this->action = $message->action;
+        $body = $message->body(); 
+        $response = $client->call('XmsRequest', ['requestData'=> $this->xms($body)]);
+        return new Response($response);
+    }
+
+
+    private function xms($body)
+    {
+        return "<xmsrequest>\n".
+                "<userid>$this->userid</userid>\n".
+                "<password>$this->password</password>\n".
+                "<action>$this->action</action>\n".
+                "<body>\n$body</body>\n".
+                '</xmsrequest>';
+    }
 }
